63e2a5c7c478a837e965ed98106050fbe9249289
rashid-shahriar <rashidslink@gmail.com>
2023-10-17 18:12:53 +0600

    JavaScript Variable Lessone 2

---
+++ 02_Variables_and_data_types/script.js
@@ -0,0 +1,29 @@
+//String
+const name = "John";
+const backticks = `Hello ${name}`;
+
+//Number
+const wholeNumber = 10;
+const decimalNumber = 10.5;
+
+// Boolean
+const isCool = true;
+
+//null
+const age = null;
+
+//undefined
+let x;
+
+//object
+const person = {
+  name: "John",
+  age: 30,
+};
+
+console.log(person);
+
+//staticlly typed language
+// c, c++, java, c#
+//Dynamiclly typed language
+// javascript, python, ruby
